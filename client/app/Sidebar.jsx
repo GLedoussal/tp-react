f9cf53b4bff98ca8d9784e9f5f461575771ecaa3
Gwendal Le Doussal <contact@gwendal-ledoussal.fr>
2017-02-09 11:51:14 +0100

    Handle events useradd & userdel

--- client/app/Sidebar.jsx
+++ client/app/Sidebar.jsx
@@ -3,16 +3,35 @@ import Drawer from 'material-ui/Drawer';
 import MenuItem from 'material-ui/MenuItem';
 import RaisedButton from 'material-ui/RaisedButton';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import ws from './Socket'
 
 // TODO get the usersList
 
-const userItems = usersList.map((user) =>
-  <MenuItem>{user.nickname}</MenuItem>
-);
-
 export default class Sidebar extends React.Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      users: []
+    };
+
+    ws.on('useradd', (user) => {
+      this.setState({
+        users: this.state.users.concat([user])
+      });
+    });
+
+    ws.on('userdel', (id) => {
+      this.setState({
+        users: this.state.users.filter((user) => user.id === id ? false : true)
+      });
+    });
+  }
+
   render() {
+    const userItems = this.state.users.map((user) =>
+      <MenuItem>{user.nickname}</MenuItem>
+    );
     return(
       <MuiThemeProvider>
         <div>
