2f269d311da5c9cf3993066cd4977e06c2f751d3
Arnaud Lefebvre <a.lefebvre@outlook.fr>
2017-02-10 16:04:45 +0100

    Sidebar: fix users refresh

--- client/app/reducers/users.js
+++ client/app/reducers/users.js
@@ -4,10 +4,10 @@ export default function(state = [], action){
   switch (action.type){
     case actions.USERS_LIST:
       return action.users;
-    case action.ADD_USER:
+    case actions.ADD_USER:
       const users = state.filter(user => user.id !== action.user.id);
       return [...users, action.user];
-    case action.DEL_USER:
+    case actions.DEL_USER:
       return state.filter(user => user.id !== action.user.id);
     default:
       return state;
